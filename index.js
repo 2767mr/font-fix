0432d73f670b8832a6200c9d8d25b347bc2c70a7
2767mr <2767mr@gmx.at>
2023-12-24 13:54:59 +0100

    try hardcoded again

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function findLineHeight(imageData, width, height, totalheight, name) {
     };
 
     if (predefined[name]) {
-        // return predefined[name];
+        return predefined[name] + 1;
     }
 
     const pixelData = imageData.data;
